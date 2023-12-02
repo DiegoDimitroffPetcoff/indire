ffdb623d46aefb3dd4a1a942914988bdf885e5e7
Diego Dimitroff Petcoff <diegodimitroffpetcoff@hotmail.com>
2023-12-02 02:05:07 +0000

    Add Styles Files and Bootstrap

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -1,5 +1,10 @@
+import  Logo  from "../../assets/INDIRE_LOGO.png";
 export const Header = () =>{
     return(
-        <header>header</header>
+        <header>
+            <figure>
+                <img src={Logo}alt="Indire Logo" style={{width: "50px"}}/>
+            </figure>
+        </header>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Header } from "../components/Common/Header";
 import { SideBar } from "../components/Common/SideBar";
 import { ProjectListByDate } from "../components/Projects/ProjectList/ProjectListByDate";
-
+import "../styles/global.css";
 export const Home = () => {
   return (
     <>      
