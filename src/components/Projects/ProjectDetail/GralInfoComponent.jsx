4d6f9a5e5b3737b7294f4f18d84b890e2ffdc091
Diego Dimitroff Petcoff <diegodimitroffpetcoff@hotmail.com>
2023-12-11 14:41:38 +0000

    Create GralInfoMocks & GralInfoComponet. Connect Boths FIle Succ.

--- src/components/Projects/ProjectDetail/GralInfoComponent.jsx
+++ src/components/Projects/ProjectDetail/GralInfoComponent.jsx
@@ -1,12 +1,14 @@
-export const GralInfoComponent = () => {
+import  LogoImage  from "../../../assets/INDIRE_LOGO.PNG"
+export const GralInfoComponent = ({GralInfoMock}) => {
+
   return <article>
-  <figure>LOGO INDIRE</figure>
-  <h1>TItle</h1>
-  <address>Adress</address>
-  <img src="imgn" alt="imagen"/>
-  <h2>Subtitle</h2>
-  <span>Date</span>
-  <p>Ref Namber</p>
+  <figure><img src={LogoImage} alt="INDIRE LOGO"/></figure>
+  <h1>{GralInfoMock.title}</h1>
+  <address>{GralInfoMock.address}</address>
+  <img src={GralInfoMock.main_img_url} alt="imagen"/>
+  <h2>{GralInfoMock.sub_title}</h2>
+  <span>{GralInfoMock.date}</span>
+  <p>{GralInfoMock.project_number}</p>
   
    </article>;
 };
--- src/pages/ProjectDitail.jsx
+++ src/pages/ProjectDitail.jsx
@@ -1,10 +1,10 @@
 import { GralInfoComponent } from "../components/Projects/ProjectDetail/GralInfoComponent";
-
+import  GralInfoMock  from "../mocks/GralInfoMock.json"
 export const ProjectDitailPage = () =>{
     return(
         <>
         Project Page
-        <GralInfoComponent/>
+        <GralInfoComponent GralInfoMock={GralInfoMock} />
         </>
     )
 }
