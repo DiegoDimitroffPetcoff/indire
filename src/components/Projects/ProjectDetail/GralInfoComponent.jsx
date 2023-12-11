2e7a44d7bf0be16812076f1921c9f790b8b19e80
Diego Dimitroff Petcoff <diegodimitroffpetcoff@hotmail.com>
2023-12-11 14:51:02 +0000

    Add Boostrap test styles

--- src/components/Projects/ProjectDetail/GralInfoComponent.jsx
+++ src/components/Projects/ProjectDetail/GralInfoComponent.jsx
@@ -1,14 +1,17 @@
-import  LogoImage  from "../../../assets/INDIRE_LOGO.PNG"
-export const GralInfoComponent = ({GralInfoMock}) => {
-
-  return <article>
-  <figure><img src={LogoImage} alt="INDIRE LOGO"/></figure>
-  <h1>{GralInfoMock.title}</h1>
-  <address>{GralInfoMock.address}</address>
-  <img src={GralInfoMock.main_img_url} alt="imagen"/>
-  <h2>{GralInfoMock.sub_title}</h2>
-  <span>{GralInfoMock.date}</span>
-  <p>{GralInfoMock.project_number}</p>
-  
-   </article>;
+import LogoImage from "../../../assets/INDIRE_LOGO.PNG";
+import  "./ProjectDetail.css";
+export const GralInfoComponent = ({ GralInfoMock }) => {
+  return (
+    <article>
+      <figure>
+        <img src={LogoImage} alt="INDIRE LOGO" />
+      </figure>
+      <h1>{GralInfoMock.title}</h1>
+      <address>{GralInfoMock.address}</address>
+      <img src={GralInfoMock.main_img_url} alt="imagen" />
+      <h2>{GralInfoMock.sub_title}</h2>
+      <span>{GralInfoMock.date}</span>
+      <p>{GralInfoMock.project_number}</p>
+    </article>
+  );
 };
--- src/pages/ProjectDitail.jsx
+++ src/pages/ProjectDitail.jsx
@@ -1,10 +1,12 @@
 import { GralInfoComponent } from "../components/Projects/ProjectDetail/GralInfoComponent";
-import  GralInfoMock  from "../mocks/GralInfoMock.json"
-export const ProjectDitailPage = () =>{
-    return(
-        <>
-        Project Page
+import GralInfoMock from "../mocks/GralInfoMock.json";
+export const ProjectDitailPage = () => {
+  return (
+    <>
+      <div class="container">
+        ProjectDitailPage
         <GralInfoComponent GralInfoMock={GralInfoMock} />
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
